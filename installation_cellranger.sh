dacad6a92fd207fe33a2310ec2a2201adf95125e
adam  boussif <aboussif@pedago-ngs.univ-lyon1.fr>
2025-03-11 03:12:06 +0100

    ajout Gemcode

--- installation_cellranger.sh
+++ installation_cellranger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Création des répertoires
-mkdir -p data/ProbeSet data/Chromium data/Telemetry data/refdata
+mkdir -p data/ProbeSet data/Chromium data/Telemetry data/refdata data/Gemcode
 
 # Téléchargement de Cell Ranger
 wget -O cellranger-9.0.1.tar.gz "https://cf.10xgenomics.com/releases/cell-exp/cellranger-9.0.1.tar.gz?Expires=1741699305&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA&Signature=Q1r~NioAyD134OyBkvDgPsUVQhxEgmir3KEokX3uqsv5WwM1E8zM36B63c-nCgxHylyQDAlPeyLnbEWU7Ix04IddviPyPekun3h1DKxnTy6NXsjoohSsDDSUC0fxhlH2j~OANZ51ADbkftLE29bR2Ghjkgdb1TuPmRvh-KYP9eM4yquaDPg5In9YkZYbCATZM-tHRNJIESBX77Bg1oCPD~RbrZCL3e-~ctwv3Ul5IOcIffmxy6RfYkVsozIvQXkE5gcU8NmOqpjVhZjWZ~yX9kipkvJ7BII6-kHF7jHx1-qzD88~ZY0qRlTiHGXZOSDjXIBTsYEHPYa-gurhdBfw4g__"
@@ -24,6 +24,10 @@ wget -P data/Chromium "https://cf.10xgenomics.com/supp/cell-vdj/Chromium-i7-Mult
 
 # Téléchargement du fichier Telemetry
 wget -P data/Telemetry "https://www.10xgenomics.com/support/software/cell-ranger/latest/tutorials/cr-pipeline-telemetry"
+
+# Téléchargement des fichiers Gemcode
+wget -P data/Gemcode "https://cf.10xgenomics.com/supp/cell-exp/gemcode-single-cell-sample-indexes-plate.csv"
+wget -P data/Gemcode "https://cf.10xgenomics.com/supp/cell-exp/gemcode-single-cell-sample-indexes-plate.json"
 
 # Extraction des archives
 tar -xzvf cellranger-9.0.1.tar.gz
